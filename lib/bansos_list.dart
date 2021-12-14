2ce8bae3a662ae16fc39317610072b778480b72e
rashadaziz <rashad.aziz@ui.ac.id>
2021-12-14 16:31:17 +0700

    updated dummy db

--- lib/bansos_list.dart
+++ lib/bansos_list.dart
@@ -30,13 +30,50 @@ class BansosList extends StatefulWidget {
 }
 
 class _BansosListState extends State<BansosList> {
+  Timer? _timer;
   var allBansosID =
       getUserBansos(); // later this function will accept user parameter
+  bool updateAvailable = false;
 
   Future<void> refreshPage() async {
     setState(() {
       allBansosID = getUserBansos();
+      updateAvailable = false;
     });
+  }
+
+  @override
+  void initState() {
+    _timer = Timer(Duration(minutes: 1), () {
+      checkForUpdates();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    if (_timer != null) {
+      _timer!.cancel();
+      _timer = null;
+    }
+    super.dispose();
+  }
+
+  void checkForUpdates() async {
+    List<String> maybeNewBansos = await getUserBansos();
+    List<String> output1 = maybeNewBansos
+        .where((element) => !allBansosID.contains(element))
+        .toList();
+    List<String> output2 = allBansosID
+        .where((element) => !maybeNewBansos.contains(element))
+        .toList();
+
+    if ((output2.isNotEmpty || output1.isNotEmpty) &&
+        updateAvailable == false) {
+      setState(() {
+        updateAvailable = true;
+      });
+    }
   }
 
   @override
@@ -54,33 +91,63 @@ class _BansosListState extends State<BansosList> {
                 snapshot.hasData) {
               List<Bansos> bansosID = snapshot.data! as List<Bansos>;
               return ListView.builder(
-                  itemCount: bansosID.length,
+                  itemCount: bansosID.length + 1,
                   itemBuilder: (context, index) {
-                    return Card(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        elevation: 5,
-                        child: ListTile(
+                    if (index == 0) {
+                      return Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(top: 10),
+                            child: const Text(
+                              "Choose Bansos to Report",
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                  decoration: TextDecoration.underline),
+                            ),
+                          ),
+                          updateAvailable
+                              ? Container(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: const [
+                                      Icon(Icons.arrow_downward_outlined),
+                                      Text(
+                                          "You have new Bansos Arriving, Pull Down To Refresh")
+                                    ],
+                                  ),
+                                )
+                              : const Text("")
+                        ],
+                      );
+                    } else {
+                      return Card(
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(20)),
-                          subtitle: Text("click to view details"),
-                          title: Text("BANSOS ${bansosID[index]}",
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.blue)),
-                          trailing: const Icon(
-                            Icons.arrow_right,
-                            color: Colors.blue,
-                          ),
-                          tileColor: Colors.blue[100],
-                          onTap: () => {
-                            // Navigator.push(
-                            //     context,
-                            //     MaterialPageRoute(
-                            //         builder: (context) =>
-                            //             BansosDetails(bansos: bansosID[index])))
-                          }, // route to details page based on the bansosID
-                        ));
+                          elevation: 5,
+                          child: ListTile(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            subtitle: Text("click to view details"),
+                            title: Text("BANSOS ${bansosID[index - 1]}",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue)),
+                            trailing: const Icon(
+                              Icons.arrow_right,
+                              color: Colors.blue,
+                            ),
+                            tileColor: Colors.blue[100],
+                            onTap: () => {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => BansosDetails(
+                                          bansos: bansosID[index - 1])))
+                            }, // route to details page based on the bansosID
+                          ));
+                    }
                   });
             } else {
               return const Center(
--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class MyApp extends StatelessWidget {
             ])),
             centerTitle: true,
           ),
-          body: BansosList()),
+          body: const BansosList()),
     );
   }
 }
